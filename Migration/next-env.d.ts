c8c588b95ab07ff0617758e6df39c2d46ce4f3af
taeginote <ab315601@naver.com>
2024-01-11 00:23:58 +0900

    📚Migration 하기전에 정의 공부

---
+++ Migration/next-env.d.ts
@@ -0,0 +1,10 @@
+//이 파일은 Next.JS 프로젝트에서 TS를 사용하기 위한 설정 파일이다.
+//이 파일은 NEXT.JS의 기본 기능과 이미지 타입에 대한 TS 타입 정의를 참조한다.
+
+/// <reference types="next" />   // => 타입스크립트의 트리플-슬래시 지시자 문법
+/// <reference types="next/image-types/global" />
+
+//이 파일은 주석에 나와있듯 파일을 수정하지 않아도된다. 그리고 TS는 v4.5.2이상을 사용하라고 권한다
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.

f5369c8d49df00184e2f46620b476296abe8faba
DevTeddy123 <ab315601@naver.com>
2025-04-25 23:54:15 +0900

    Fix : build 오류 해결

--- app-router/src/app/(with-searchbar)/layout.tsx
+++ app-router/src/app/(with-searchbar)/layout.tsx
@@ -1,10 +1,12 @@
-import { ReactNode } from 'react'
+import { ReactNode, Suspense } from 'react'
 import SearchBox from '@/components/search-box'
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<div>
-			<SearchBox />
+			<Suspense fallback={<div>로딩중....</div>}>
+				<SearchBox />
+			</Suspense>
 			{children}
 		</div>
 	)

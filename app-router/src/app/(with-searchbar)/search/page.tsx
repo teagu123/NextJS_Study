f5654b3c9277794a1dc3fe71418ab3fcc8e5dcc3
DevTeddy123 <ab315601@naver.com>
2025-04-26 15:15:53 +0900

    Feat : 동적 메타데이터

--- app-router/src/app/(with-searchbar)/search/page.tsx
+++ app-router/src/app/(with-searchbar)/search/page.tsx
@@ -1,7 +1,36 @@
 import BookItem from '@/components/book-item'
 import { BookData } from '@/types'
 import Delay from '@/utils/delay'
+import { Metadata } from 'next'
 import { Suspense } from 'react'
+
+export const metadata: Metadata = {
+	title: 'ex 타이틀',
+	description: 'ex description',
+	openGraph: {
+		title: 'ex title',
+		description: 'ex description',
+		images: ['/주소'],
+	},
+}
+
+export async function generateMetadata({
+	searchParams,
+}: {
+	searchParams: Promise<{ q?: string }>
+}): Promise<Metadata> {
+	//현재 페이지의 메타데이터를 동적으로 처리 가능
+	const { q } = await searchParams
+	return {
+		title: `${q} : 검색`,
+		description: `${q} 검색 결과 입니다.`,
+		openGraph: {
+			title: `${q} title`,
+			description: 'ex description',
+			images: ['/주소'],
+		},
+	}
+}
 
 export default async function Page({
 	searchParams,
